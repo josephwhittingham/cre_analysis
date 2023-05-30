d6685a17d42d2142e4e44cbe5e632842336de2f5
Joe Whittingham <josephwhittingham@hotmail.com>
2023-05-30 16:12:13 +0200

    Tidying up single-tracer-properties script

--- misc/get_single_tracer_properties.py
+++ misc/get_single_tracer_properties.py
@@ -2,10 +2,10 @@ import numpy as np
 import h5py
 
 for i in range(306):
-  hf3 = h5py.File("tracer_file_{:03d}.hdf5".format(i),"r")
-  idx2 = np.where(hf3['TracerData/ParticleIDs'][()] == 2000055001)
+  hf = h5py.File("tracer_file_{:03d}.hdf5".format(i),"r")
+  idx = np.where(hf['TracerData/ParticleIDs'][()] == 2000055001)
   try:
-    time_idx = np.where(hf3['TracerData/TimestepLastIndex'][()] > idx2[0][0])[0][0]
+    time_idx = np.where(hf['TracerData/TimestepLastIndex'][()] >= idx[0][0])[0][0]
   except:
-    time_idx = np.where(hf3['TracerData/TimestepLastIndex'][()] > idx2[0])[0][0]
-  print(i, hf3['TracerData/InjectionEnergy'][idx2], np.linalg.norm(np.vstack([hf3['TracerData/MagneticField/X'][idx2], hf3['TracerData/MagneticField/Y'][idx2], hf3['TracerData/MagneticField/Z'][idx2]]), axis=0), hf3['TracerData/Time'][time_idx])
+    time_idx = np.where(hf['TracerData/TimestepLastIndex'][()] >= idx[0])[0][0]
+  print(i, hf['TracerData/InjectionEnergy'][idx], np.linalg.norm(np.vstack([hf['TracerData/MagneticField/X'][idx], hf['TracerData/MagneticField/Y'][idx], hf['TracerData/MagneticField/Z'][idx]]), axis=0), hf['TracerData/Time'][time_idx])
