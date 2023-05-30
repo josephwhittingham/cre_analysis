969413f4b2b02fb56a041bb5fc2d7be7011efcfb
Joe Whittingham <josephwhittingham@hotmail.com>
2023-05-30 16:11:31 +0200

    BUGFIX: correction to time at which max num tracers is calculated

--- estimate_size/estimate_size_of_tracer_data_from_out.py
+++ estimate_size/estimate_size_of_tracer_data_from_out.py
@@ -1,4 +1,7 @@
-with open("21240.out") as file:
+# Might be further savings from compression (extra 10% typical?)
+# Need to check that data chunk sizes are accurate (how many fields are we saving?)
+
+with open("78376.out") as file:
   lines = [line.rstrip() for line in file]
 
 tot_num_tracers = 0
@@ -18,11 +21,11 @@ for i in range(len(lines)):
         no_tracers = int(elements[1])
         tot_num_tracers += int(elements[1])
         tot_num_active_timesteps += 1
+
+        if(no_tracers > max_num_tracers):           # for an estimate if we had recorded every tracer for each timestep
+          max_num_tracers = no_tracers
       else:
         tot_num_shocked_tracers += int(elements[1])
-
-  if(no_tracers > max_num_tracers):           # for an estimate if we had recorded every tracer for each timestep
-    max_num_tracers = no_tracers
 
 chunk_size = 44             # data size recorded for one tracer (4 bytes * 11 fields ) - standard data
 shocked_chunk_size = 32     # data size recorded for one tracer (4 bytes * 8 fields ) - shock acceleration data (only stored for non-zero shock flags)
