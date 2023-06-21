bf28ebde3561394d2b811f6282a5d8cb21ad5e15
Joe Whittingham <josephwhittingham@hotmail.com>
2023-06-21 15:15:01 +0200

    Updated TimestepLastIndex to NextTimestepStartIndex

--- misc/get_single_tracer_properties.py
+++ misc/get_single_tracer_properties.py
@@ -5,7 +5,7 @@ for i in range(306):
   hf = h5py.File("tracer_file_{:03d}.hdf5".format(i),"r")
   idx = np.where(hf['TracerData/ParticleIDs'][()] == 2000055001)
   try:
-    time_idx = np.where(hf['TracerData/TimestepLastIndex'][()] >= idx[0][0])[0][0]
+    time_idx = np.where(hf['TracerData/NextTimestepStartIndex'][()] >= idx[0][0])[0][0]
   except:
-    time_idx = np.where(hf['TracerData/TimestepLastIndex'][()] >= idx[0])[0][0]
+    time_idx = np.where(hf['TracerData/NextTimestepStartIndex'][()] >= idx[0])[0][0]
   print(i, hf['TracerData/InjectionEnergy'][idx], np.linalg.norm(np.vstack([hf['TracerData/MagneticField/X'][idx], hf['TracerData/MagneticField/Y'][idx], hf['TracerData/MagneticField/Z'][idx]]), axis=0), hf['TracerData/Time'][time_idx])
